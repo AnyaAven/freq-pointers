32f36bd9fb33fe09b80eb1003577454726f4bf81
Anya Aven <leafeon96@gmail.com>
2024-04-12 18:12:01 -0400

    Added createFreqCounter func and wrote psuedo code

--- can-construct-word/canConstructWord.js
+++ can-construct-word/canConstructWord.js
@@ -10,8 +10,10 @@ Cannot reuse letters
 
 function canConstructWord(word, letters) {
     //if letters is shorter than word then fail fast
+    if (letters.length < word.length) return false;
 
     //create freq counter for letters
+
 
     //loop through word
     //check to see if the char exists in the freq counter
@@ -20,6 +22,21 @@ function canConstructWord(word, letters) {
 
 
     return true;
+}
+
+/* Accepts an array or string and returns a freq counter object
+
+["a", "a", "c", "d"] -> {a: 2, c: 1, d: 1}
+*/
+function createFreqCounter(letters){
+    //create an empty object for freq counter
+
+    // loop through letters
+    // check if the char exists inside the counter
+    // if yes, increment be one,
+    // if not, set it to 0
+
+    // return freq counter obj
 }
 
 export { canConstructWord };
