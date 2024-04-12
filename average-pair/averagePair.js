03835814b30bbcb30c022a0525e46054407059ab
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-12 16:51:34 -0700

    leftPointer bug

--- average-pair/averagePair.js
+++ average-pair/averagePair.js
@@ -29,39 +29,13 @@ function averagePair(nums, targetAvg) {
             rightPointer--;
         }
         else {
-            leftPointer--;
+            leftPointer++;
         }
     }
 
-    return false
+    return false;
 
-    //While the left is less than or equal to the (array length - 2), loop.
 
-    // while (leftPointer <= nums.length - 2) {
-    //     // Check if right > nums.length - 1, if yes, then increment left by 1 and
-    //     // set right to (left + 1)
-    //     console.log({ leftPointer, rightPointer });
-
-    //     if (rightPointer > nums.length - 1) {
-    //         leftPointer++;
-    //         rightPointer = leftPointer + 1;
-
-    //     } else if ((nums[leftPointer] + nums[rightPointer]) / 2 === targetAvg) {
-    //         console.log((nums[leftPointer] + nums[rightPointer]) / 2);
-    //         //Get left val and right val then calculate average
-    //         //check if it matches the targetAvg, if yes, return true!
-
-    //         return true;
-
-    //     }
-    //     else {
-    //         // Else increment the right by one
-    //         rightPointer++;
-    //     }
-
-    // }
-
-    //return false;
 }
 
 export { averagePair };
