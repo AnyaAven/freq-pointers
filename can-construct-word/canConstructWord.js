c670812e4b9810182420bfdf85b9465829f78dca
Anya Aven <leafeon96@gmail.com>
2024-04-12 18:05:33 -0400

    Created docstring and pseudo code

--- can-construct-word/canConstructWord.js
+++ can-construct-word/canConstructWord.js
@@ -1,5 +1,25 @@
+/* Check to see if all the 'letters' can be used to create the 'word'
+
+returns a boolean
+"bcd", ["a", "b", "c", "d"] -> true
+
+Cannot reuse letters
+"aa", ["a", "b", "c", "d"] -> false
+*/
+
+
 function canConstructWord(word, letters) {
-    return false;
+    //if letters is shorter than word then fail fast
+
+    //create freq counter for letters
+
+    //loop through word
+    //check to see if the char exists in the freq counter
+    // if yes and the value is greater than 0, -1 in freq counter.
+    // if no, return false
+
+
+    return true;
 }
 
 export { canConstructWord };
