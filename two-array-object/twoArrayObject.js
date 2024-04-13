1117072f3048941831895a6375de04821048275b
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-12 17:00:10 -0700

    added in this fn

--- two-array-object/twoArrayObject.js
+++ two-array-object/twoArrayObject.js
@@ -1,5 +1,33 @@
+/**
+create an object where the keys are the first array and values
+from second array
+
+if the values array is less than keys array, set the key's value to null
+
+if the keys array is less than values array, stop at the end of keys array
+
+Examples:
+twoArrayObject(['x', 'y', 'z'], [1, 2]);
+  // {'x': 1, 'y': 2, 'z': null}
+
+twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4]);
+  // {'a': 1, 'b': 2, 'c': 3}
+*/
+
 function twoArrayObject(keys, vals) {
-    return { "": null };
+    if (keys.length === 0) {
+        return { "": null };
+    }
+
+    const keyValObj = {};
+
+    for (let i = 0; i < keys.length; i++) {
+
+        keyValObj[keys[i]] = vals[i] || null;
+
+    }
+
+    return keyValObj;
 }
 
 export { twoArrayObject };
